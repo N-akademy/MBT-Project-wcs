35855bdd81733e695dfb9b85670ffbb7b4115d8c
Cyrheel_ <cyrilb38@hotmail.com>
2022-08-29 09:21:47 +0200

    push last changes

--- client/src/Pages/Ticket/TicketListPage.tsx
+++ client/src/Pages/Ticket/TicketListPage.tsx
@@ -4,8 +4,10 @@ import FilterBar from '../../Components/Common/FilterBar';
 import FilteredTicketList from '../../Components/Ticket/FilteredTicketList';
 import { h2Style, pageWrapper } from '../../Styles/style';
 import IFilter from 'Interfaces/IFilter';
+import { useNavigate } from 'react-router-dom';
 
 function TicketListPage(): JSX.Element {
+  const navigate = useNavigate();
   const [filters, setFilters] = useState<IFilter>({
     project: 1,
     user: 1,
@@ -19,6 +21,9 @@ function TicketListPage(): JSX.Element {
     <div {...pageWrapper}>
       <Navbar />
       <h2 {...h2Style}>Your Tickets</h2>
+      <button onClick={() => navigate('/ticketCreation', { replace: true })}>
+        New Ticket +
+      </button>
       <FilterBar setFilterValue={handleFiltersValue} actualValues={filters} />
       <FilteredTicketList filters={filters} />;
     </div>
